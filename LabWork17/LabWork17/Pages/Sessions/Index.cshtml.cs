7292286a8fd10c6ff44df3cbe17677f444817d96
unV1RUS <danipate091@gmail.com>
2025-12-10 14:20:38 +0300

    lr19

--- LabWork17/LabWork17/Pages/Sessions/Index.cshtml.cs
+++ LabWork17/LabWork17/Pages/Sessions/Index.cshtml.cs
@@ -1,25 +1,63 @@
 ﻿using LabWork17.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace LabWork17.Pages.Sessions
 {
-    public class IndexModel : PageModel
+    public class IndexModel(Contexts.CinemaDbContext context) : PageModel
     {
-        private readonly LabWork17.Contexts.CinemaDbContext _context;
+        private readonly Contexts.CinemaDbContext _context = context;
 
-        public IndexModel(LabWork17.Contexts.CinemaDbContext context)
-        {
-            _context = context;
-        }
+        private const int PageSize = 2;
+
+        [BindProperty(SupportsGet = true)]
+        public string? FilmTitle { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? SortColumn { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public byte? Hall { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int PageIndex { get; set; } = 1;
+
+        public int TotalPages { get; set; }
 
         public IList<Session> Session { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
-            Session = await _context.Sessions
+            ViewData["Halls"] = new SelectList(_context.Halls, "HallId", "CinemaHallNumber");
+
+            var sessions = _context.Sessions
                 .Include(s => s.Hall)
-                .Include(s => s.Movie).ToListAsync();
+                .Include(s => s.Movie)
+                .AsQueryable();
+
+            if (!String.IsNullOrEmpty(FilmTitle))
+                sessions = sessions.Where(m => m.Movie.Name.Contains(FilmTitle));
+
+            switch (SortColumn)
+            {
+                case "price":
+                    sessions = sessions.OrderBy(s => s.Price);
+                    break;
+                case "price_desc":
+                    sessions = sessions.OrderByDescending(s => s.Price);
+                    break;
+            }
+
+            if (Hall > 0)
+                sessions = sessions.Where(h => h.Hall.HallId == Hall);
+
+            //TotalPages = Math.Ceiling(sessions.Count() / PageSize);
+
+            sessions = sessions.Skip((PageIndex - 1) * PageSize).Take(PageSize);
+
+            Session = await sessions.ToListAsync();
         }
     }
 }
